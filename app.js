244506c703fba698802610b66863f980fca4bdf9
Tony Nyarangi <antony.momanyi@gmail.com>
2019-03-22 10:18:14 +0300

    allowing cors

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser');
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var bodyParser = require("body-parser");
 
 port = process.env.PORT || 3001;
-
 
 let database = require("./app/models/database");
 
@@ -15,5 +15,6 @@ app.listen(port);
 console.log("API server started on: " + port);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 var routes = require("./app/routes/appRoutes"); //importing route
 routes(app); //register the route
