f65a4573f8b8e6d10083e1b66b4e61be108f5705
Tony Nyarangi <antony.momanyi@gmail.com>
2019-05-01 12:37:13 +0300

    .

--- app/models/notification.js
+++ app/models/notification.js
@@ -1,5 +1,5 @@
 const options = {
-  apiKey: "2cb0f4582034bad0b91eca8f4ff9bc0f03aa6dc1a6a2a43704a70823b3de4509", // use your sandbox app API key for development in the test environment
+  apiKey: "4ba11d7dbc8848511ca38760c73cc7f126611acedd728218c93684467e49a2dc", // use your sandbox app API key for development in the test environment
   username: "sandbox" // use 'sandbox' for development in the test environment
 };
 const Africastalking = require("africastalking")(options);
@@ -14,7 +14,7 @@ sms = Africastalking.SMS;
 // }
 const caseID = 8959;
 const messageoptions = {
-  to: [`+254715898985`],
+  to: [`+254793720223`],
   message: `A match for your case id ${caseID} has been found, pairedke.`
 };
 
